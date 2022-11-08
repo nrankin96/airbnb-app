5bbaa434a8e0116d75ff92e7d6fa88253c63aeef
nrankin96 <nykiarankin96@gmail.com>
2022-11-08 00:11:26 -0500

    formatting header

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,24 @@
 
-import Image from 'next/image';
+import Image from 'next/legacy/image';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { Search } from '@mui/icons-material';
 
 function Header() {
     return ( 
-        <header>
-        {/* left */ } 
-         <div className="relative flex items-center h-10 cursor-pointer">
-               <Image 
-                src={"https://links.papareact.com/qd3"}
-                layout="fill"
-                objectFit="contain"
-                objectPosition="left"
-               />
-            </div>
+        // Left
+      <header className='sticky top-0 z-50 my-auto grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
+      <div className='relative flex items-center h-10 cursor-pointer'>
+      <Image src={"https://links.papareact.com/qd3"} layout="fill" objectFit='contain' objectPosition='left'/>
 
-         {/* middle */ } 
-         <div></div>
+      </div>
+      {/* Middle = Search */}
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+        <input className='pl-5 transparent outline-none flex-grow' type='text' placeholder='Start your search'/>
+        <MagnifyingGlassIcon className='h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer hidden md:inline-flex'/>
 
-         {/* right */ } 
-         <div></div>
-
-        </header>
+      </div>
+      </header>
+      
     )
 }
 
