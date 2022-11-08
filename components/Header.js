3b42ea4c453789952fd925c68ae6443cf5c1893c
nrankin96 <nykiarankin96@gmail.com>
2022-11-07 23:06:09 -0500

    building Header

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+
+import Image from 'next/image';
+
+function Header() {
+    return ( 
+        <header>
+        {/* left */ } 
+         <div className="relative flex items-center h-10 cursor-pointer">
+               <Image 
+                src={"https://links.papareact.com/qd3"}
+                layout="fill"
+                objectFit="contain"
+                objectPosition="left"
+               />
+            </div>
+
+         {/* middle */ } 
+         <div></div>
+
+         {/* right */ } 
+         <div></div>
+
+        </header>
+    )
+}
+
+export default Header;
